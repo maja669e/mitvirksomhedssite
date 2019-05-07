d44e80fa4d97e0da691b9655fb3f41cb6106065c
Fiona Wray <fionawray@laptop-2044hci2.kea.local>
2019-05-07 11:26:10 +0200

    Prøver burger, virker ikke endnu

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function sidenVises() {
     console.log("sidenVises");
     document.querySelector("#parfume").classList.add("move");
     document.querySelector("#parfume").addEventListener("animationend", imageAppear);
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
 function imageAppear() {
@@ -14,4 +16,18 @@ function imageAppear() {
     document.querySelector("#bluecloud_container").classList.add("move");
     document.querySelector("#bluecloud_container").addEventListener("animationend", sidenVises);
 
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult =
+        document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
 }
