242b5b6cefe8408213604a10ea16da68a6280e80
Fiona Wray <fionawray@laptop-2044hci2.kea.local>
2019-05-07 11:17:26 +0200

    forside

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("sidenVises");
     document.querySelector("#parfume").classList.add("move");
+    document.querySelector("#parfume").addEventListener("animationend", imageAppear);
 }
 
 function imageAppear() {
     console.log("imageAppear");
+
+    document.querySelector("#parfume").removeEventListener("move");
+    document.querySelector("#parfume").classList.remove("move");
+    document.querySelector("#bluecloud_container").classList.add("move");
+    document.querySelector("#bluecloud_container").addEventListener("animationend", sidenVises);
 
 }
