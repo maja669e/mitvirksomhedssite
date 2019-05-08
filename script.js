1df2f8afdc3c4cb7bf160cb858759b2dbd848e31
fran663a <fran663a@stud.kea.dk>
2019-05-08 12:54:01 +0200

    Der er intet ændret her

--- script.js
+++ script.js
@@ -47,3 +47,23 @@ function infoSkaerm1() {
     document.querySelector("#info_skaerm1").classList.remove("hidden");
     document.querySelector("#kryds1").addEventListener("click", perfumeScreen);
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
