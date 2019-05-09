dbb726c742d43e45dff7845a5815009e7622707f
Fiona Wray <fionawray@laptop-0u61ues8.kea.local>
2019-05-09 08:52:00 +0200

    .

---
+++ scripts2.js
@@ -0,0 +1,62 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+
+}
+document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult =
+        document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
+}
+
+
+function perfumeScreen() {
+    console.log("perfumeScreen");
+
+    if (document.getElementById("pink")) {
+        document.querySelector("#info_skaerm1").classList.add("hidden");
+        document.querySelector("#pink").addEventListener("click", infoSkaerm1);
+    }
+}
+
+
+function infoSkaerm1() {
+    console.log("infoSkaerm1");
+    document.querySelector("#info_skaerm1").classList.remove("hidden");
+    document.querySelector("#kryds1").addEventListener("click", perfumeScreen);
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
